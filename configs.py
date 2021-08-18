3f30bace9030ca51bb50e0185f98a7d082c67768
Kevin Blin <kblin@outlook.com>
2021-08-18 11:03:01 +0200

    mv textgan floder to src

--- configs.py
+++
@@ -1,8 +0,0 @@
-cuda_device = "cuda:0"
-current_dataset = 'fashion_mnist'  # 'mnist' / 'fashion_mnist' / 'cifar10'
-
-fid_samples_location = '/localhome/kucharav/FID_samples'
-training_samples_location = '/localhome/kucharav/trainer_samples'
-
-# CURRENTPASS: configure the map from the dataset to the usual parameters that get autoloaded
-#  into parameters.
